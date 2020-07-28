e67143911f53bbeb87c7a8d3df180edd3ff3e65f
phelippemac <phelippemac@hotmail.com>
2020-07-28 12:29:27 -0300

    Alter Strong Params

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -47,6 +47,6 @@ class ContactsController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def contact_params
-    params.require(:contact).permit(:name, :email, :birthdate)
+    params.require(:contact).permit(:name, :email, :birthdate, :kind_id)
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,20 @@
 class Contact < ApplicationRecord
   belongs_to :kindness
+
   def author
     'João Phelippe'
   end
+
+  def kind_description
+    kindness.description
+  end
+
+  def as_json(_options = {})
+    super(
+      root: true,
+      methods: %i[author kind_description]
+      # include: {kindness: {only: :description} }
+    )
+  end
 end
+ 
